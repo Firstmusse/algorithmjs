3ccf4f354ae930968d75aa1d9eec12a501cf78c5
Alexander Stepanenko <mussefirst1@gmail.com>
2022-04-13 18:20:55 +0600

    Алгоритм фибоначи

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector('button').onclick = myClick
+
+function myClick() {
+    
+    let a = document.querySelector('.i-1').value
+    console.log(a);
+    document.querySelector('.out').innerHTML = a
+}
